621205eb6f9daf04e9dbae65c2e9655e0f8da540
Idaho Frost <frostidaho@gmail.com>
2017-01-24 17:46:54 -0500

    Add xlib_edid.py file for reading EDID data from x11

---
+++ src/edider/xlib_edid.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+from Xlib import X, display, Xatom
+from Xlib.ext import randr
+
+from contextlib import contextmanager
+
+@contextmanager
+def get_window(i_screen=0):
+    "Create & manage a x-window."
+    screen = display.Display().screen(i_screen)
+    window = screen.root.create_window(0, 0, 1, 1, 1, screen.root_depth)
+    yield window
+    window.destroy()
+
+def get_output_id(window):
+    "Return the primary output (int) for a given x-window"
+    primary_out = randr.get_output_primary(window)
+    primary_out = primary_out.output
+    return primary_out
+
+def get_output_edid(i_screen=0):
+    "Return the raw EDID for a given screen."
+    PROPERTY_EDID = display.Display().intern_atom('EDID', only_if_exists=True)
+    with get_window(i_screen) as window:
+        outp = get_output_id(window)
+        # props = randr.list_output_properties(window, outp)
+        edid = randr.get_output_property(window, outp, PROPERTY_EDID,
+                                         Xatom.INTEGER, 0, 400)
+        edid = bytes(edid.value)
+    return edid
+
+
+if __name__ == '__main__':
+    for i in range(display.Display().screen_count()):
+        edid = get_output_edid(i)
+        print('Screen {}'.format(i))
+        print(edid)
+
