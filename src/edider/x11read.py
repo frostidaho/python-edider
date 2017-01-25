026c2670ed2c9bd2c7e2fec8da7daaca45f0bf60
Idaho Frost <frostidaho@gmail.com>
2017-01-24 21:11:51 -0500

    Add X11Screen class to x11read.py

--- src/edider/x11read.py
+++ src/edider/x11read.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from Xlib import X, display, Xatom
 from Xlib.ext import randr
-
+from edider.parser import BaseScreen
 from contextlib import contextmanager
 
 @contextmanager
@@ -33,9 +33,29 @@ def get_screen_resolution(i_screen=0):
     scr = display.Display().screen(i_screen)
     return scr.width_in_pixels, scr.height_in_pixels
 
+class X11Screen(BaseScreen):
+    def __init__(self, index=0):
+        """Index is the X11 index of the screen"""
+        self._id = index
+
+    @property
+    def edid(self):
+        try:
+            return self._edid
+        except AttributeError:
+            self._edid = get_output_edid(self._id)
+            return self._edid
+        
+    def _get_resolution(self):
+        x,y = get_screen_resolution(self._id)
+        self._width_in_pixels, self._height_in_pixels = int(x), int(y)
+
+
 if __name__ == '__main__':
     for i in range(display.Display().screen_count()):
-        edid = get_output_edid(i)
-        print('Screen {}'.format(i))
-        print(edid)
+        s = X11Screen(i)
+        print(s)
+        print(s.misc)
+        print(s.edid)
+        print(40*'-')
 
