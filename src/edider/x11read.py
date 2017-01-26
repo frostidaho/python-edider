bff1c700367a7e3af320cd0a6da96c3c1ae3b231
Idaho Frost <frostidaho@gmail.com>
2017-01-25 21:59:11 -0500

    Remove comments from x11read

--- src/edider/x11read.py
+++ src/edider/x11read.py
@@ -6,7 +6,6 @@ from edider.parser import BaseScreen
 from contextlib import contextmanager
 from collections import namedtuple
 
-# OutputInfo = namedtuple('OutputInfo', ('edid', 'name', 'props'))
 
 @contextmanager
 def get_window(i_screen=0):
@@ -111,26 +110,6 @@ class X11Output:
         cname = self.__class__.__name__
         return cname + '({})'.format(self.idx)
 
-# def get_output_info(*output_ids, i_screen=0):
-#     "Return the raw EDID for a given screen."
-#     PROPERTY_EDID = display.Display().intern_atom('EDID', only_if_exists=True)
-#     with get_window(i_screen) as window:
-#         for outp in output_ids:
-#             # props = randr.list_output_properties(window, outp)
-#             props = randr.get_output_info(window, outp, 0)
-#             edid = randr.get_output_property(window, outp, PROPERTY_EDID,
-#                                              Xatom.INTEGER, 0, 400)
-#             edid = bytes(edid.value)
-#             yield OutputInfo(
-#                 edid=edid,
-#                 props=props,
-#                 name=props.name,
-#             )
-#             # yield edid, props
-
-# def get_screen_resolution(i_screen=0):
-#     scr = display.Display().screen(i_screen)
-#     return scr.width_in_pixels, scr.height_in_pixels
 
 class Monitor(BaseScreen):
     def __init__(self, index):
