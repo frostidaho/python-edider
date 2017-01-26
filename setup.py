684233b663522127daf08e7ff736591ccfc91b70
Idaho Frost <frostidaho@gmail.com>
2017-01-26 16:31:00 -0500

    Add python-xlib dependency to setup.py

--- setup.py
+++ setup.py
@@ -65,6 +65,7 @@ setup(
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
     install_requires=[
+        'python-xlib',
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
     ],
     extras_require={
